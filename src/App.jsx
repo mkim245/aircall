7104a8f7583f7cbb6b2ee9982c58b2a4cfcb6881
Mingu Kim <mkim245@gmail.com>
2023-01-23 22:47:44 -0500

    use axios to get data from api and updated react and reactDom

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from './Header.jsx';
 import Navbar from './Navbar.jsx';
 import Topbox from './Topbox.jsx';
 import Bottom from './Bottom.jsx';
-import { Getapi } from './Getapi';
+import axios from 'axios';
 
 const App = () => {
-  // useEffect(() => {
-  //   Getapi(`channels?part=snippet&id=${id}`)
-  //     .then((data) => setChannelDetail(data?.items[0]));
-  //   fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
-  //     .then((data) => setVideos(data?.items));
-  // }, [id])
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    axios.get(`https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities`)
+      .then((res) => {
+        console.log("****", res.data)
+        setContacts(res.data);
+      })
+      .catch((error) => console.log("error---", error));
+  }, []);
+
   return (
     <div className='container'>
-      <Header/>
-      <Navbar/>
-      <Topbox/>
-      <div className="container-view">Some activities should be here</div>
-      <Bottom/>
+      <Header />
+      <Navbar />
+      <Topbox />
+      <div className="container-view">
+        {contacts.map((contact) => (
+                <h1>{contact.id}</h1>
+              ))}
+      </div>
+      <Bottom />
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 export default App;
