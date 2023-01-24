e8ffa2dcf7f455f33a6c1c0e541719edd041c4a5
Mingu Kim <mkim245@gmail.com>
2023-01-24 13:01:42 -0500

    show missing calls

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,42 @@ const App = () => {
       .catch((error) => console.log("error---", error));
   }, []);
 
+  function dateChange(date) {
+    let date_part = date.split("-");
+    let month = '';
+    let newdate = '';
+    if (date_part[1] === '01') month = 'January';
+    if (date_part[1] === '02') month = 'Feburary';
+    if (date_part[1] === '03') month = 'March';
+    if (date_part[1] === '04') month = 'April';
+    if (date_part[1] === '05') month = 'May';
+    if (date_part[1] === '06') month = 'June';
+    if (date_part[1] === '07') month = 'July';
+    if (date_part[1] === '08') month = 'August';
+    if (date_part[1] === '09') month = 'September';
+    if (date_part[1] === '10') month = 'October';
+    if (date_part[1] === '11') month = 'November';
+    if (date_part[1] === '12') month =' December';
+    return newdate = month + ',' + ' ' + date_part[2] + ' ' + date_part[0];
+  }
+
+  function timeTo12HrFormat(time) {   // Take a time in 24 hour format and format it in 12 hour format
+    var time_part_array = time.split(":");
+    var ampm = 'AM';
+
+    if (time_part_array[0] >= 12) {
+      ampm = 'PM';
+    }
+
+    if (time_part_array[0] > 12) {
+      time_part_array[0] = time_part_array[0] - 12;
+    }
+
+    var formatted_time = time_part_array[0] + ':' + time_part_array[1] + ' ' + ampm;
+
+    return formatted_time;
+  }
+
   return (
     <div className='container'>
       <Header />
@@ -26,8 +62,18 @@ const App = () => {
       <Topbox />
       <div className="container-view">
         {contacts.map((contact) => (
-                <h1>{contact.id}</h1>
-              ))}
+          // fragment is not working<></>
+          contact.call_type === "missed"
+            ? <React.Fragment>
+              <div>----{dateChange((contact.created_at).split('T')[0])}----</div>
+              <span>📲</span>
+              <span>
+                <span>{contact.from}</span> <span>tried to call on Xavior</span>
+              </span>
+              <span>{timeTo12HrFormat(((contact.created_at).split('T')[1]).split('.')[0])}</span>
+            </React.Fragment>
+            : <React.Fragment></React.Fragment>
+        ))}
       </div>
       <Bottom />
     </div>
