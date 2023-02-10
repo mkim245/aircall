f9ce465e3f60b8a92eadea842862002b240b8ec0
Mingu Kim <mkim245@gmail.com>
2023-02-10 18:42:08 -0500

    make ibox and allcalls

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,24 @@ import Navbar from './Navbar.jsx';
 import Topbox from './Topbox.jsx';
 import Bottom from './Bottom.jsx';
 import Getapi from './Getapi.jsx';
+import Allcalls from './Allcalls.jsx';
+import Inbox from './Inbox.jsx';
 
 const App = () => {
+  const [inbox, setInbox] = React.useState(false)
+  const [allcalls, setAllcalls] = React.useState(false)
 
   return (
     <div className='container'>
       <Header />
       <Navbar />
       <Topbox />
-      <Getapi />
+      {/* {!inbox && !allcalls && <Getapi />} */}
+      {inbox && !allcalls && <Inbox />}     
+      {!inbox && allcalls && <Allcalls />}
+      {/* <Inbox /> */}
+      {/* {!inbox && allcalls && <Getapi />}
+      {!allcalls && inbox && <Getapi />} */}
       <Bottom />
     </div>
   );
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import Getapi from './Getapi.jsx';
 
 const Navbar = () => {
+  const handleClickInbox = function (event) {
+    event.preventDefault();
+    alert("You clicked inbox");
+    Getapi();
+  };
+
+  const handleClickAll = function (event) {
+    event.preventDefault();
+    alert("You clicked all");
+    Getapi();
+  };
+
   return (
     <dir className="navbar">
       <span className="activity">📞 Activity </span>
       <span className="submenu">
-        <span className="inbox">Inbox </span>
+        <span className="inbox" onClick={handleClickInbox}>Inbox </span>
         <span>:</span>
-        <span className="allcalls">All calls</span>
+        <span className="allcalls" onClick={handleClickAll}>All calls</span>
         <span>:</span>
         <span>⚙️</span>
       </span>
