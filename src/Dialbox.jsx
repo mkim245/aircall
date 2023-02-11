18c91cf586174c890a4be8427b978e0dd75c4f0d
Mingu Kim <mkim245@gmail.com>
2023-02-10 23:15:46 -0500

    style dialbox

--- src/Dialbox.jsx
+++ src/Dialbox.jsx
@@ -3,9 +3,22 @@ import React from 'react'
 const Dialbox = () => {
   return (
     <div className="container-sub">
-
-  </div>
-)
+      <div className="dial">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+          <div>*</div>      
+          <div>0</div>
+          <div>#</div>          
+      </div>
+    </div>
+  )
 }
 
 export default Dialbox
