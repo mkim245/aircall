9b2f2a326a80b3e713889975e3d23f3e15e7658a
Mingu Kim <mkim245@gmail.com>
2023-02-11 13:30:11 -0500

    display dials

--- src/Dialbox.jsx
+++ src/Dialbox.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 
 const Dialbox = () => {
+  const [current, setCurrent] = React.useState('');
+  const append = function (number) {
+    setCurrent (number);
+  }
+
   return (
     <div className="container-sub">
       <div className="dial">
-        <div className="display"></div>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
+        <div className="display">
+          <div className="dialnumber">{current}</div>
+        </div>
+          <div onClick={() => append('1')}>1</div>
+          <div onClick={() => append('2')}>2</div>
+          <div onClick={() => append('3')}>3</div>
+          <div onClick={() => append('4')}>4</div>
+          <div onClick={() => append('5')}>5</div>
+          <div onClick={() => append('6')}>6</div>
+          <div onClick={() => append('7')}>7</div>
+          <div onClick={() => append('8')}>8</div>
+          <div onClick={() => append('9')}>9</div>
           <div>*</div>
-          <div>0</div>
+          <div onClick={() => append('0')}>0</div>
           <div>#</div>
 
       </div>
