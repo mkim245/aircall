2da8dad5de41d9d7a18b3c62152b2ad1f3426987
Mingu Kim <mkim245@gmail.com>
2023-02-11 15:40:29 -0500

    add mui package and style dialbox

--- src/Dialbox.jsx
+++ src/Dialbox.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import LocalPhoneSharpIcon from '@mui/icons-material/LocalPhoneSharp';
 
 const Dialbox = () => {
   const [current, setCurrent] = React.useState('');
   const append = function (number) {
-    setCurrent (number);
+    setCurrent(`${current}` + number);
   }
 
   return (
@@ -12,19 +13,21 @@ const Dialbox = () => {
         <div className="display">
           <div className="dialnumber">{current}</div>
         </div>
-          <div onClick={() => append('1')}>1</div>
-          <div onClick={() => append('2')}>2</div>
-          <div onClick={() => append('3')}>3</div>
-          <div onClick={() => append('4')}>4</div>
-          <div onClick={() => append('5')}>5</div>
-          <div onClick={() => append('6')}>6</div>
-          <div onClick={() => append('7')}>7</div>
-          <div onClick={() => append('8')}>8</div>
-          <div onClick={() => append('9')}>9</div>
-          <div>*</div>
-          <div onClick={() => append('0')}>0</div>
-          <div>#</div>
-
+        <div onClick={() => append('1')}>1</div>
+        <div onClick={() => append('2')}>2</div>
+        <div onClick={() => append('3')}>3</div>
+        <div onClick={() => append('4')}>4</div>
+        <div onClick={() => append('5')}>5</div>
+        <div onClick={() => append('6')}>6</div>
+        <div onClick={() => append('7')}>7</div>
+        <div onClick={() => append('8')}>8</div>
+        <div onClick={() => append('9')}>9</div>
+        <div>*</div>
+        <div onClick={() => append('0')}>0</div>
+        <div>#</div>
+      </div>
+      <div className="calling">
+        <LocalPhoneSharpIcon sx={{ fontSize: 50, "color": "green"}}></LocalPhoneSharpIcon>
       </div>
     </div>
   )
