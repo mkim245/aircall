fb211b1f65d5e78b72c99aff027ab3adfd506aa8
Mingu Kim <mkim245@gmail.com>
2023-02-12 01:45:31 -0500

    add backspaceicon

--- src/Dialbox.jsx
+++ src/Dialbox.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import LocalPhoneSharpIcon from '@mui/icons-material/LocalPhoneSharp';
+import BackspaceIcon from '@mui/icons-material/Backspace';
 
 const Dialbox = () => {
   const [current, setCurrent] = React.useState('');
@@ -22,12 +23,14 @@ const Dialbox = () => {
         <div onClick={() => append('7')}>7</div>
         <div onClick={() => append('8')}>8</div>
         <div onClick={() => append('9')}>9</div>
-        <div>*</div>
+        <div>✳</div>
         <div onClick={() => append('0')}>0</div>
         <div>#</div>
-      </div>
-      <div className="calling">
-        <LocalPhoneSharpIcon sx={{ fontSize: 50, "color": "green"}}></LocalPhoneSharpIcon>
+        <div></div>
+        <div className="calling">
+          <LocalPhoneSharpIcon sx={{ fontSize: 50, "color": "white", "background-color": "green", "border-radius": "50%", "padding": "7px 7px" }}></LocalPhoneSharpIcon>
+        </div>
+        <div><BackspaceIcon></BackspaceIcon></div>
       </div>
     </div>
   )
